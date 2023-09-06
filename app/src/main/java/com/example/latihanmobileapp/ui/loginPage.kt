58f7c4e9ed0ecf3d21ef0362201b93db0a88e607
unknown <adigum17@gmail.com>
2023-09-06 18:23:04 +0700

    init

---
+++ app/src/main/java/com/example/latihanmobileapp/ui/loginPage.kt
@@ -0,0 +1,12 @@
+package com.example.latihanmobileapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.latihanmobileapp.R
+
+class loginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
---
+++ app/src/main/java/com/example/latihanmobileapp/ui/registPage.kt
@@ -0,0 +1,12 @@
+package com.example.latihanmobileapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.latihanmobileapp.R
+
+class registPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist_page)
+    }
+}
