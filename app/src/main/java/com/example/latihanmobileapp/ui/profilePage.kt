0bd3200620f26eb7435aa7d47bc7e27b8769a6a1
acer <adigum17@gmail.com>
2023-12-14 15:49:37 +0700

    Navigasi Navbar

--- app/src/main/java/com/example/latihanmobileapp/ui/profilePage.kt
+++ app/src/main/java/com/example/latihanmobileapp/ui/profilePage.kt
@@ -1,25 +1,15 @@
 package com.example.latihanmobileapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
-import android.widget.Button
+import android.os.Bundle
 import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.latihanmobileapp.R
 
-class homePage : AppCompatActivity() {
+class profilePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-        val lihatSemua = findViewById<TextView>(R.id.LihatSemua)
-
-        lihatSemua.setOnClickListener(){
-            val intent = Intent(this, rekomendasiPage::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
-        }
+        setContentView(R.layout.activity_profile)
 
         // Inisialisasi tombol-tombol dari navigation bar
         val btnHome : LinearLayout = findViewById(R.id.btnHome)
