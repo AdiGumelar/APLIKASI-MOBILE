f4a2c5d82d543cee1ab53d674d9d137558d1e3e4
acer <adigum17@gmail.com>
2023-12-14 15:56:44 +0700

    Navigasi Navbar

--- app/src/main/java/com/example/latihanmobileapp/ui/rekomendasiPage.kt
+++ app/src/main/java/com/example/latihanmobileapp/ui/rekomendasiPage.kt
@@ -1,7 +1,9 @@
 package com.example.latihanmobileapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -43,5 +45,31 @@ class rekomendasiPage : AppCompatActivity() {
                 // Handle failure
             }
         })
+
+        // Inisialisasi tombol-tombol dari navigation bar
+        val btnHome : LinearLayout = findViewById(R.id.btnHome)
+        val btnCart : LinearLayout = findViewById(R.id.btnCart)
+        val btnProfile : LinearLayout = findViewById(R.id.btnProfile)
+
+        // Menangani klik pada tombol Home
+        btnHome.setOnClickListener {
+            val intent = Intent(this, homePage::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+        }
+
+        // Menangani klik pada tombol Cart
+        btnCart.setOnClickListener {
+            val intent = Intent(this, rekomendasiPage::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+        }
+
+        // Menangani klik pada tombol Profile
+        btnProfile.setOnClickListener {
+            val intent = Intent(this, profilePage::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out)
+        }
     }
 }
